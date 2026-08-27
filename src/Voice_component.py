9e837eca442c31c8ed33c307381cfd915f4b6ec7
rajasekar0467-max <rajasekar0467@gmail.com>
2026-08-27 19:58:02 +0530

    Create Voice_component.py

---
+++ src/Voice_component.py
@@ -0,0 +1,12 @@
+import os
+import streamlit.components.v1 as components
+
+_component_dir = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "voice_component_frontend"
+)
+
+voice_assistant_component = components.declare_component(
+    "tourist_ai_voice",
+    path=_component_dir
+)
