e6d1513c79fb853fc8dd8a49c16ba275e8e8648a
rajasekar0467-max <rajasekar0467@gmail.com>
2026-08-27 20:08:22 +0530

    Update voice_component.py

--- src/voice_component.py
+++ src/voice_component.py
@@ -1,12 +1,20 @@
 import os
 import streamlit.components.v1 as components
 
-_component_dir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+COMPONENT_DIR = os.path.join(
+    BASE_DIR,
     "voice_component_frontend"
 )
 
+
 voice_assistant_component = components.declare_component(
     "tourist_ai_voice",
-    path=_component_dir
+    path=COMPONENT_DIR
 )
