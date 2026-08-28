d97d543e4195e126ea9cff6d7e6016f803a4ae81
rajasekar0467-max <rajasekar0467@gmail.com>
2026-08-28 18:42:40 +0530

    Update voice_component.py

--- src/voice_component.py
+++ src/voice_component.py
@@ -2,38 +2,14 @@ import os
 import streamlit.components.v1 as components
 
 
-# src/voice_component.py
-# Project root = parent of src folder
-BASE_DIR = os.path.dirname(
-    os.path.dirname(
-        os.path.abspath(__file__)
-    )
+CURRENT_DIR = os.path.dirname(
+    os.path.abspath(__file__)
 )
 
 COMPONENT_DIR = os.path.join(
-    BASE_DIR,
+    CURRENT_DIR,
     "voice_component_frontend"
 )
-
-
-# Helpful error instead of mysterious Streamlit error
-if not os.path.isdir(COMPONENT_DIR):
-    raise FileNotFoundError(
-        "voice_component_frontend folder not found.\n"
-        f"Expected location: {COMPONENT_DIR}"
-    )
-
-
-INDEX_FILE = os.path.join(
-    COMPONENT_DIR,
-    "index.html"
-)
-
-if not os.path.isfile(INDEX_FILE):
-    raise FileNotFoundError(
-        "index.html not found.\n"
-        f"Expected location: {INDEX_FILE}"
-    )
 
 
 voice_assistant_component = components.declare_component(
